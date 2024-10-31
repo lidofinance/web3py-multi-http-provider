44e69d9952bdf3e21a44aae74a5ce4a29c7debf7
Ryan Collingham <ryan.collingham@blockscholes.com>
2024-10-31 13:04:32 +0000

    rename and add imports to __init__.py

--- web3_multi_provider/__init__.py
+++ web3_multi_provider/__init__.py
@@ -1,6 +1,14 @@
 from .multi_http_provider import (
     FallbackProvider,
     MultiProvider,
+)
+
+from .async_multi_http_provider import (
+    AsyncFallbackProvider,
+    AsyncMultiProvider,
+)
+
+from .exceptions import (
     NoActiveProviderError,
     ProtocolNotSupported,
 )
@@ -8,6 +16,8 @@ from .multi_http_provider import (
 __all__ = (
     "FallbackProvider",
     "MultiProvider",
+    "AsyncFallbackProvider",
+    "AsyncMultiProvider",
     "NoActiveProviderError",
     "ProtocolNotSupported",
 )
